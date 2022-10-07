138c0d3c49691e0ce03a000c6ca827c5e0f1ef52
dragosstan2602 <dragos.stan2602@gmail.com>
2022-10-07 16:32:18 +0200

    Add files via upload

---
+++ config/constants.py
@@ -0,0 +1,140 @@
+workouts = {
+    "triceps": [
+        {
+            "name": "dumbbell_overhead",
+			"link": "https://www.bodybuilding.com/content/the-10-best-muscle-building-triceps-exercises.html#the-10-best-muscle-building-triceps-exercises-cable-overhead-triceps-extension"
+        },
+        {
+            "name": "cable_overhead",
+			"link": "https://www.bodybuilding.com/content/the-10-best-muscle-building-triceps-exercises.html#the-10-best-muscle-building-triceps-exercises-seated-triceps-press"
+        },
+        {
+            "name": "cable_kickback_single",
+			"link": "https://www.bodybuilding.com/content/the-10-best-muscle-building-triceps-exercises.html#-the-10-best-muscle-building-triceps-exercises-cable-triceps-kick-back"
+        },
+        {
+            "name": "cable_push_down",
+			"link": "https://www.bodybuilding.com/content/the-10-best-muscle-building-triceps-exercises.html#the-10-best-muscle-building-triceps-exercises-cable-rope-push-down"
+        },
+        {
+            "name": "bench_dip",
+			"link": "https://www.bodybuilding.com/content/the-10-best-muscle-building-triceps-exercises.html#the-10-best-muscle-building-triceps-exercises-bench-dip"
+        },
+        {
+            "name": "triceps_machine_dip",
+			"link": "https://www.bodybuilding.com/content/the-10-best-muscle-building-triceps-exercises.html#the-10-best-muscle-building-triceps-exercises-triceps-dip-machine"
+        }
+    ],
+    "biceps": [
+        {
+            "name": "cable_curl",
+			"link": "https://www.bodybuilding.com/content/10-best-muscle-building-biceps-exercises.html#10-best-biceps-exercises-for-building-muscle-standing-cable-curl"
+        },
+        {
+            "name": "conc_curl",
+			"link": "https://www.bodybuilding.com/content/10-best-muscle-building-biceps-exercises.html#10-best-biceps-exercises-for-building-muscle-concentration-curl"
+        },
+        {
+            "name": "dumbbell_curl",
+			"link": "https://www.bodybuilding.com/content/10-best-muscle-building-biceps-exercises.html#10-best-biceps-exercises-for-building-muscle-rope-hammer-curl"
+        },
+        {
+            "name": "chin_up",
+			"link": "https://www.bodybuilding.com/content/10-best-muscle-building-biceps-exercises.html#10-best-biceps-exercises-for-building-muscle-weighted-chin-up"
+        },
+        {
+            "name": "ezbar_curl",
+			"link": "https://www.bodybuilding.com/content/10-best-muscle-building-biceps-exercises.html#standing-barbell-curl"
+        }
+    ],
+    "shoulders": [
+        {
+            "name": "arnold_press",
+			"link": "https://www.bodybuilding.com/content/10-best-muscle-building-shoulder-exercises.html#10-best-muscle-building-shoulder-exercises-arnold-press"
+        },
+        {
+            "name": "dumbbell_press",
+			"link": "https://www.bodybuilding.com/content/10-best-muscle-building-shoulder-exercises.html#10-best-muscle-building-shoulder-exercises-seated-dumbell-press"
+        },
+        {
+            "name": "lateral_raise",
+			"link": "https://www.bodybuilding.com/content/10-best-muscle-building-shoulder-exercises.html#10-best-muscle-building-shoulder-exercises-lateral-raise"
+        },
+        {
+            "name": "front_raise",
+			"link": "https://www.bodybuilding.com/content/10-best-muscle-building-shoulder-exercises.html#10-best-muscle-building-shoulder-exercises-front-raise"
+        },
+        {
+            "name": "hanging",
+            "link": ""
+        }
+    ],
+    "back": [
+        {
+            "name": "pull_ups",
+			"link": "https://www.bodybuilding.com/content/10-best-muscle-building-back-exercises.html#top-back-exercises-for-building-muscle-pull-ups"
+        },
+        {
+            "name": "lat_pull_down",
+			"link": "https://www.bodybuilding.com/content/10-best-muscle-building-back-exercises.html#top-back-exercises-for-building-muscle-lat-pull-down"
+        },
+        {
+            "name": "single_dumbbell_row",
+			"link": "https://www.bodybuilding.com/content/10-best-muscle-building-back-exercises.html#top-back-exercises-for-building-muscle-single-arm-dumbbell-row"
+        },
+        {
+            "name": "seated_row",
+			"link": "https://www.bodybuilding.com/content/10-best-muscle-building-back-exercises.html#top-back-exercises-for-building-muscle-seated-cable-row"
+        }
+    ],
+    "chest": [
+        {
+            "name": "push_ups",
+            "link": ""
+        },
+        {
+            "name": "machine_fly",
+			"link": "https://www.bodybuilding.com/content/10-best-chest-exercises-for-building-muscle.html#10-best-chest-exercises-for-building-muscle-pec-deck-machine"
+        },
+        {
+            "name": "machine_press",
+			"link": "https://www.bodybuilding.com/content/10-best-chest-exercises-for-building-muscle.html#10-best-chest-exercises-for-building-muscle-seated-machine-press"
+        },
+        {
+            "name": "dumbbell_press",
+            "link": "https://www.bodybuilding.com/content/10-best-chest-exercises-for-building-muscle.html#10-best-chest-exercises-for-building-muscle-dumbbell-bench-press"
+        }
+    ],
+    "legs": [
+        {
+            "name": "squats",
+            "link": ""
+        },
+        {
+            "name": "lunges",
+            "link": ""
+        },
+        {
+            "name": "calves",
+            "link": ""
+        }
+    ],
+    "abs": [
+        {
+            "name": "russian_twist",
+            "link": ""
+        },
+        {
+            "name": "crunches",
+            "link": ""
+        },
+        {
+            "name": "plank",
+            "link": ""
+        },
+        {
+            "name": "ab_rollout",
+            "link": ""
+        }
+    ]
+}
---
+++ run.py
@@ -0,0 +1,99 @@
+from fastapi import FastAPI, HTTPException, status, Body
+from fastapi.responses import Response, JSONResponse
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel, Field
+from datetime import datetime
+from pymongo import MongoClient
+from bson import ObjectId
+import config.constants as C
+import jmespath
+from typing import List
+import os
+
+app = FastAPI()
+
+client = MongoClient(
+    host="localhost",
+    port=27017,
+    username=os.environ("MONGODB_USER"),
+    password=os.environ("MONGODB_PASS")
+)
+db = client.workouts
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
+class WorkoutModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    muscle_group: str = Field(...)
+    workout_type: str = Field(...)
+    reps: int = Field(...)
+    weight: int = Field(...)
+    date: datetime = datetime.utcnow()
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "muscle_group": "chest",
+                "workout_type": "push-ups",
+                "reps": 12,
+                "weight": 0,
+                "date": "2022-10-03T17:06:16.848605"
+            }
+        }
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/workout/{muscle_group}",  response_description="List all workouts", response_model=List[WorkoutModel])
+async def read_item(muscle_group: str):
+    if muscle_group not in C.workouts.keys():
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"{muscle_group} not valid muscle group")
+
+    query = {"muscle_group": muscle_group}
+
+    workouts = db["workouts"].find(query)
+
+    return [item for item in workouts]
+
+    # raise HTTPException(status_code=404, detail=f"No {id} workouts found")
+
+
+@app.post("/workout", response_description="Add new workout", response_model=WorkoutModel)
+async def create_workout(workout: WorkoutModel = Body(...)):
+    workout = jsonable_encoder(workout)
+    if workout['muscle_group'] not in C.workouts.keys():
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"{workout['muscle_group']} not valid muscle group")
+
+    valid_exercises = jmespath.search(f"{workout['muscle_group']}[*].name",
+                                      C.workouts)
+
+    if workout['workout_type'] not in valid_exercises:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"{workout['workout_type']} not valid workout type for {workout['muscle_group']}")
+        
+    new_workout = db["workouts"].insert_one(workout)
+    created_workout = db["workouts"].find_one({"_id": new_workout.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_workout)
